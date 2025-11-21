81db89f43ee293dbaa3167439c8f4af2ca05891a
CristianRedin <kristianredin@gmail.com>
2025-11-21 02:41:12 -0500

    Puerto 4000

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 8560
+EXPOSE 4000
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def home():
     return render_template_string(HTML, mensaje=mensaje)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8560)
+    app.run(host="0.0.0.0", port=4000)
